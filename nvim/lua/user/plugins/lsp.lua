6da96e0ff4fa1035af0d7906068520e3e127f2f4
AVAniketh0905 <adimoolamaniketh@gmail.com>
2026-03-21 18:32:02 +0530

    added support for espidf

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -1,37 +1,40 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				pyright = {
-					settings = {
-						python = {
-							analysis = {
-								typeCheckingMode = "strict",
-								autoSearchPaths = true,
-								useLibraryCodeForTypes = true,
-								diagnosticMode = "workspace",
-							},
-						},
-					},
-				},
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "strict",
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = "workspace",
+              },
+            },
+          },
+        },
 
-				gopls = {},
-				lua_ls = {},
+        gopls = {},
+        lua_ls = {},
 
-				clangd = {
-					cmd = {
-						"clangd",
-						"--pretty",
-						"--header-insertion=iwyu",
-						"--background-index",
-						"--clang-tidy",
-						"--completion-style=detailed",
-						"--header-insertion=never",
-						"--compile-commands-dir=build",
-					},
-				},
-			},
-		},
-	},
+        clangd = {
+          cmd = {
+            "clangd",
+            "--pretty",
+            "--header-insertion=iwyu",
+            "--background-index",
+            "--clang-tidy",
+            "--completion-style=detailed",
+            "--header-insertion=never",
+            "--compile-commands-dir=build",
+
+            -- for espressif projects
+            "--query-driver=/home/avaniketh0905/.espressif/tools/**/bin/*gcc",
+          },
+        },
+      },
+    },
+  },
 }
