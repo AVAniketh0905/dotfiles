35baa23e9b9f6946eedcd3b50ffcb64ae02d4c51
AVAniketh0905 <adimoolamaniketh@gmail.com>
2026-06-19 14:21:48 +0530

    added snippets and spellcheck

---
+++ nvim/lua/user/plugins/spellcheck.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "markdown", "text" },
+        callback = function()
+          vim.opt_local.spell = true
+          vim.opt_local.spelllang = { "en_us" }
+        end,
+      })
+    end,
+  },
+}
